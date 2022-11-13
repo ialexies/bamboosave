3029b06333739be603bb639a61bf885f5745c569
alex <ialexies@gmail.com>
2022-11-14 07:39:00 +0800

    Added flutterfire Ui

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutterfire_ui/auth.dart';
 import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
@@ -9,12 +13,35 @@ class AppPages {
   AppPages._();
 
   static const INITIAL = Routes.HOME;
-
+  static const providerConfigs = [EmailProviderConfiguration()];
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
     ),
+    GetPage(
+        name: _Paths.signin,
+        page: () {
+          return SignInScreen(
+            providerConfigs: providerConfigs,
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                final FirebaseAuth auth = FirebaseAuth.instance;
+                final User? user = auth.currentUser;
+                final uid = user?.uid;
+
+                FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(uid)
+                    .update({})
+                    .then((value) => print("User Details"))
+                    .catchError((error) => print("Failed to add user: $error"));
+
+                Navigator.pushReplacementNamed(context, '/cryptos');
+              }),
+            ],
+          );
+        }),
   ];
 }
