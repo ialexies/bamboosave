1fdbc903faf76ba53d1f581d19a707c94f3a3d44
alex <ialexies@gmail.com>
2022-11-13 21:41:03 +0800

    clean codes of getx pattern

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -5,16 +5,20 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:bamboosave/app/routes/app_pages.dart';
 import 'package:bamboosave/counter/counter.dart';
 import 'package:bamboosave/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
